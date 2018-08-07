08dd08016ef226fee2c9c1a2d569ab0820e9b7e2
Alin Mihailescu <alin.d.mihailescu@gmail.com>
2018-08-07 17:20:30 +0300

    Add files via upload
    VB.net app to confirm orders in SAP system

---
+++ OrderConf/OrderConf/Form1.Designer.vb
@@ -0,0 +1,297 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Start = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.PasteButton = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ClearButtonList1 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.CloseButton = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.CopyButtonList1 = New System.Windows.Forms.Button()
+        Me.ClearButtonList2 = New System.Windows.Forms.Button()
+        Me.CopyButtonList2 = New System.Windows.Forms.Button()
+        Me.CopyButtonList3 = New System.Windows.Forms.Button()
+        Me.ClearButtonList3 = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Start
+        '
+        Me.Start.Location = New System.Drawing.Point(372, 514)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(170, 40)
+        Me.Start.TabIndex = 20
+        Me.Start.Text = "Start"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(14, 107)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(170, 355)
+        Me.ListBox1.TabIndex = 25
+        '
+        'PasteButton
+        '
+        Me.PasteButton.Location = New System.Drawing.Point(14, 466)
+        Me.PasteButton.Name = "PasteButton"
+        Me.PasteButton.Size = New System.Drawing.Size(170, 40)
+        Me.PasteButton.TabIndex = 26
+        Me.PasteButton.Text = "Paste content"
+        Me.PasteButton.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(11, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(154, 17)
+        Me.Label3.TabIndex = 27
+        Me.Label3.Text = "Orders to be confirmed"
+        '
+        'ListBox2
+        '
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.Location = New System.Drawing.Point(191, 107)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(170, 355)
+        Me.ListBox2.TabIndex = 28
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(188, 87)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(174, 17)
+        Me.Label4.TabIndex = 29
+        Me.Label4.Text = "Operation times confirmed"
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(372, 107)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(170, 355)
+        Me.ListBox3.TabIndex = 30
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(369, 87)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(118, 17)
+        Me.Label5.TabIndex = 31
+        Me.Label5.Text = "Orders confirmed"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ClearButtonList1)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 90)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(172, 463)
+        Me.GroupBox1.TabIndex = 32
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'ClearButtonList1
+        '
+        Me.ClearButtonList1.Location = New System.Drawing.Point(102, 423)
+        Me.ClearButtonList1.Name = "ClearButtonList1"
+        Me.ClearButtonList1.Size = New System.Drawing.Size(68, 40)
+        Me.ClearButtonList1.TabIndex = 42
+        Me.ClearButtonList1.Text = "Clear All"
+        Me.ClearButtonList1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Location = New System.Drawing.Point(191, 90)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(170, 372)
+        Me.GroupBox2.TabIndex = 33
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "GroupBox2"
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Location = New System.Drawing.Point(372, 87)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(170, 375)
+        Me.GroupBox3.TabIndex = 34
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "GroupBox3"
+        '
+        'CloseButton
+        '
+        Me.CloseButton.Location = New System.Drawing.Point(192, 514)
+        Me.CloseButton.Name = "CloseButton"
+        Me.CloseButton.Size = New System.Drawing.Size(170, 40)
+        Me.CloseButton.TabIndex = 35
+        Me.CloseButton.Text = "Close"
+        Me.CloseButton.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(9, 9)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(530, 46)
+        Me.Label6.TabIndex = 36
+        Me.Label6.Text = "Insert Orders and press Start"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(12, 55)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(337, 25)
+        Me.Label7.TabIndex = 37
+        Me.Label7.Text = "Select text and press ""Paste Content"""
+        '
+        'CopyButtonList1
+        '
+        Me.CopyButtonList1.Location = New System.Drawing.Point(14, 513)
+        Me.CopyButtonList1.Name = "CopyButtonList1"
+        Me.CopyButtonList1.Size = New System.Drawing.Size(68, 40)
+        Me.CopyButtonList1.TabIndex = 39
+        Me.CopyButtonList1.Text = "Copy"
+        Me.CopyButtonList1.UseVisualStyleBackColor = True
+        '
+        'ClearButtonList2
+        '
+        Me.ClearButtonList2.Location = New System.Drawing.Point(294, 466)
+        Me.ClearButtonList2.Name = "ClearButtonList2"
+        Me.ClearButtonList2.Size = New System.Drawing.Size(68, 42)
+        Me.ClearButtonList2.TabIndex = 40
+        Me.ClearButtonList2.Text = "Clear All"
+        Me.ClearButtonList2.UseVisualStyleBackColor = True
+        '
+        'CopyButtonList2
+        '
+        Me.CopyButtonList2.Location = New System.Drawing.Point(192, 466)
+        Me.CopyButtonList2.Name = "CopyButtonList2"
+        Me.CopyButtonList2.Size = New System.Drawing.Size(68, 42)
+        Me.CopyButtonList2.TabIndex = 41
+        Me.CopyButtonList2.Text = "Copy"
+        Me.CopyButtonList2.UseVisualStyleBackColor = True
+        '
+        'CopyButtonList3
+        '
+        Me.CopyButtonList3.Location = New System.Drawing.Point(372, 466)
+        Me.CopyButtonList3.Name = "CopyButtonList3"
+        Me.CopyButtonList3.Size = New System.Drawing.Size(68, 42)
+        Me.CopyButtonList3.TabIndex = 44
+        Me.CopyButtonList3.Text = "Copy"
+        Me.CopyButtonList3.UseVisualStyleBackColor = True
+        '
+        'ClearButtonList3
+        '
+        Me.ClearButtonList3.Location = New System.Drawing.Point(474, 465)
+        Me.ClearButtonList3.Name = "ClearButtonList3"
+        Me.ClearButtonList3.Size = New System.Drawing.Size(68, 42)
+        Me.ClearButtonList3.TabIndex = 43
+        Me.ClearButtonList3.Text = "Clear All"
+        Me.ClearButtonList3.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(554, 24)
+        Me.MenuStrip1.TabIndex = 45
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.ClientSize = New System.Drawing.Size(554, 582)
+        Me.Controls.Add(Me.CopyButtonList3)
+        Me.Controls.Add(Me.ClearButtonList3)
+        Me.Controls.Add(Me.CopyButtonList2)
+        Me.Controls.Add(Me.ClearButtonList2)
+        Me.Controls.Add(Me.CopyButtonList1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.CloseButton)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.PasteButton)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Start)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximumSize = New System.Drawing.Size(570, 620)
+        Me.MinimumSize = New System.Drawing.Size(570, 620)
+        Me.Name = "Form1"
+        Me.Text = "Order Confirmation App"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Start As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents PasteButton As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents CloseButton As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents CopyButtonList1 As Button
+    Friend WithEvents ClearButtonList2 As Button
+    Friend WithEvents CopyButtonList2 As Button
+    Friend WithEvents ClearButtonList1 As Button
+    Friend WithEvents CopyButtonList3 As Button
+    Friend WithEvents ClearButtonList3 As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+End Class
---
+++ OrderConf/OrderConf/Form1.vb
@@ -0,0 +1,286 @@
+﻿Imports System.Text
+
+Public Class Form1
+
+    Dim i As Integer
+
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
+
+        Dim session As Object
+        'Dim order As String
+        Dim counter As Integer = 0
+
+        'connect to session
+        Try
+            session = SessionConnect()
+            'go to time confirmation transaction
+            GoToTimeConfirmation(session)
+
+            ' step to confirm times from list 1
+            ' reading from list 1 
+            For i = 0 To ListBox1.Items.Count - 1
+
+                'Try to confirm item
+                'move to TimeConfirm function, if the result of confirmation is true move the order to next list
+                If TimeConfirm(ListBox1.Items(i - counter).ToString, session) = True Then
+                    ListBox1.GetSelected(i - counter)
+                    ListBox2.Items.Add(ListBox1.Items(i - counter))
+                    ListBox1.Items.RemoveAt(i - counter)
+                    counter = counter + 1
+                End If
+            Next i
+
+            'step to close order from list2
+            'reading list2 items
+            'if items on list go further, else stop
+            If ListBox2.Items.Count > 0 Then
+                'use counter to update the number of items in the list
+                counter = 0
+
+                'go to order confirmation transaction
+                GoToOrderConfirmation(session)
+                ' Going from list 2 to list 3
+                For i = 0 To ListBox2.Items.Count - 1
+                    'Try To confirm item
+                    'move to OrderConfirm function, if the result of confirmation is true move the order to next list
+                    If OrderConfirm(ListBox2.Items(i - counter), session) = True Then
+                        ListBox2.GetSelected(i - counter)
+                        ListBox3.Items.Add(ListBox2.Items(i - counter))
+                        ListBox2.Items.RemoveAt(i - counter)
+                        counter = counter + 1
+                    End If
+                Next i
+
+            End If
+        Catch ex As Exception
+            MsgBox("Conecteaza-te!")
+
+        End Try
+
+    End Sub
+
+    Function GoToTimeConfirmation(ByVal session As Object)
+
+        'go to transaction CO11n
+        Try
+            session.findById("wnd[0]").resizeWorkingPane(247, 39, False)
+            session.findById("wnd[0]/tbar[0]/okcd").text = "/nco11n"
+            session.findById("wnd[0]").sendVKey(0)
+        Catch ex As Exception
+            MsgBox("Conecteaza-te!")
+            Return 0
+        End Try
+
+    End Function
+
+    Function GoToOrderConfirmation(ByVal session As Object)
+
+        'go to transaction CO02
+        Try
+            session.findById("wnd[0]").resizeWorkingPane(247, 39, False)
+            session.findById("wnd[0]/tbar[0]/okcd").text = "/nco02"
+            session.findById("wnd[0]").sendVKey(0)
+        Catch ex As Exception
+            MsgBox("Conecteaza-te!")
+            Return 0
+        End Try
+    End Function
+
+
+
+
+    'time confirm function
+    'receives session and order params
+    'returns true or false depending of the result of the confirmation
+    Function TimeConfirm(ByVal order As String, session As Object) As Boolean
+        'TimeStatus param to evaluate the result of the confirmation
+        Dim TimeStatus As Boolean = False
+        'OperationExist to evaluate if other operation exist
+        Dim OperationExist As Boolean = True
+
+        Dim SaveSucces As String = ""
+
+        'params to check number of operated cycles
+        Dim CheckNumberOfOperations As Integer = 0
+        Dim NumberOfOperations As Integer = 0
+
+        'make at least one try to evaluate the order
+        Do
+            NumberOfOperations = NumberOfOperations + 1
+            session.findById("wnd[0]/usr/ssubSUB01:SAPLCORU_S:0010/subSLOT_HDR:SAPLCORU_S:0117/ctxtAFRUD-AUFNR").text = order
+            session.findById("wnd[0]/usr/ssubSUB01:SAPLCORU_S:0010/subSLOT_HDR:SAPLCORU_S:0117/ctxtAFRUD-VORNR").setFocus
+            session.findById("wnd[0]/usr/ssubSUB01:SAPLCORU_S:0010/subSLOT_HDR:SAPLCORU_S:0117/ctxtAFRUD-VORNR").caretPosition = 0
+            session.findById("wnd[0]").sendVKey(4)
+
+            'if window apeared evaluate the type of
+            If session.ActiveWindow.Name = "wnd[1]" Then
+                'OperationExist sau comanda nu exista
+                session.findById("wnd[1]/tbar[0]/btn[0]").press
+                session.findById("wnd[0]").sendVKey(0)
+                session.findById("wnd[0]/tbar[0]/btn[11]").press
+
+                'construct the status bar message for succes 
+                SaveSucces = "Confirmare c-dă " + order + " salvată"
+                If session.FindById("wnd[0]/sbar").Text = SaveSucces Then
+                    OperationExist = True
+                    CheckNumberOfOperations = CheckNumberOfOperations + 1
+                Else
+                    OperationExist = False
+                End If
+                ' if there are nu values the order has been confirmated before running program
+            ElseIf session.FindById("wnd[0]/sbar").Text = "Nu au fost găsite valori" Then
+                OperationExist = False
+                CheckNumberOfOperations = CheckNumberOfOperations + 1
+
+                'any other error make exit the lood
+            Else
+                OperationExist = False
+            End If
+        Loop While OperationExist
+
+        'evaluate if there are more than one operation confimed
+        If CheckNumberOfOperations = 0 Then
+            TimeStatus = False
+        ElseIf CheckNumberOfOperations = NumberOfOperations - 1 Then
+            TimeStatus = True
+        ElseIf CheckNumberOfOperations = NumberOfOperations Then
+            TimeStatus = True
+        Else
+            TimeStatus = False
+        End If
+
+        Return TimeStatus
+
+    End Function
+
+    Function OrderConfirm(ByVal order As String, session As Object) As Boolean
+        'OrderStatus param to evaluate the result of the confirmation
+        Dim OrderStatus As Boolean = False
+
+        'if there is an order go to confirmation
+        If order <> "" Then
+            session.findById("wnd[0]/usr/ctxtCAUFVD-AUFNR").text = order
+            session.findById("wnd[0]/usr/ctxtCAUFVD-AUFNR").caretPosition = 9
+            session.findById("wnd[0]").sendVKey(0)
+            'if wnd[1] appears order allready confirmed
+            If session.ActiveWindow.Name = "wnd[1]" Then
+                session.findById("wnd[1]/tbar[0]/btn[0]").press
+                session.findById("wnd[0]/tbar[0]/btn[11]").press
+                OrderStatus = False
+            Else
+                'confirm the order
+                session.findById("wnd[0]/mbar/menu[1]/menu[9]/menu[3]").select
+                session.findById("wnd[0]/tbar[0]/btn[11]").press
+                session.findById("wnd[0]/usr/ctxtCAUFVD-AUFNR").text = ""
+                session.findById("wnd[0]/usr/ctxtCAUFVD-AUFNR").caretPosition = 0
+                OrderStatus = True
+            End If
+        End If
+        Return OrderStatus
+    End Function
+
+    'connect to SAP Object
+    Function SessionConnect() As Object
+
+        Dim SapGuiAuto As Object
+        Dim Application As Object
+        Dim connection As Object
+        Dim session As Object
+        Try
+            SapGuiAuto = GetObject("SAPGUI")
+            Application = SapGuiAuto.GetScriptingEngine
+            connection = Application.Children(0)
+            session = connection.Children(0)
+            session.findById("wnd[0]").resizeWorkingPane(247, 39, False)
+            Return session
+        Catch ex As Exception
+            MsgBox("Conecteaza-te!")
+            Return 0
+        End Try
+        Return session
+    End Function
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles PasteButton.Click
+        Try
+            Dim s As String = Clipboard.GetText()
+            Dim lines() As String = s.Split
+            For Each line In lines
+                If line.Length > 0 Then
+                    ListBox1.Items.Add(line.Trim)
+                End If
+
+            Next
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles CopyButtonList1.Click
+
+        Try
+            Dim sb As New StringBuilder
+            For Each row In ListBox1.SelectedItem
+                sb.Append(row.ToString)
+                'sb.AppendLine()
+            Next
+            Clipboard.SetData(DataFormats.Text, sb.ToString)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles CopyButtonList2.Click
+        Try
+            Dim sb As New StringBuilder
+            For Each row In ListBox2.SelectedItem
+                sb.Append(row.ToString)
+                'sb.AppendLine()
+            Next
+            Clipboard.SetData(DataFormats.Text, sb.ToString)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles CopyButtonList3.Click
+        Try
+            Dim sb As New StringBuilder
+            For Each row In ListBox3.SelectedItem
+                sb.Append(row.ToString)
+                'sb.AppendLine()
+            Next
+            Clipboard.SetData(DataFormats.Text, sb.ToString)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles ClearButtonList1.Click
+        ListBox1.Items.Clear()
+    End Sub
+
+    Private Sub ClearButtonList2_Click(sender As Object, e As EventArgs) Handles ClearButtonList2.Click
+        ListBox2.Items.Clear()
+    End Sub
+
+    Private Sub ClearButtonList3_Click(sender As Object, e As EventArgs) Handles ClearButtonList3.Click
+        ListBox3.Items.Clear()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles CloseButton.Click
+        Me.Close()
+    End Sub
+End Class
